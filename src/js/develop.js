d74d52475447e43e13486f3d1a58c73fbc9bb266
tampledog <tampledog@gmail.com>
2018-03-09 16:27:54 +0200

    first push

---
+++ src/js/develop.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+   $('.js-screen-slider').slick({
+       arrows: false,
+       infinite: true,
+       dots: false,
+       slidesToShow: 5,
+       slidesToScroll: 3
+   });
+});
